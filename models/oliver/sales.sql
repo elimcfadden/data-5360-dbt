1b748786ff3b0d30734a72cd72f891fcf16a5511
Eli McFadden <a02331666@aggies.usu.edu>
2025-10-19 19:44:54 +0000

    Commit

--- models/oliver/sales.sql
+++ models/oliver/sales.sql
@@ -15,5 +15,48 @@ from {{ ref('fact_sales') }} f
 left join {{ ref('oliver_dim_customer') }} c on f.customer_key = c.customer_key
 left join {{ ref('oliver_dim_employee') }} e on f.employee_key = e.employee_key
 left join {{ ref('oliver_dim_store') }}    s on f.store_key    = s.store_key
+
+
+
+
+
+
+
+
+
+
+
+models
+/
+oliver
+/
+sales.sql
+
+Save
+name,
+  d.date_day,
+  f.quantity,
+  f.unit_price,
+  f.line_amount
+from {{ ref('fact_sales') \}\} f
+left join {{ ref('oliver_dim_customer') \}\} c on f.customer_key = c.customer_key
+left join {{ ref('oliver_dim_employee') \}\} e on f.employee_key = e.employee_key
+left join {{ ref('oliver_dim_store') \}\}    s on f.store_key    = s.store_key
+left join {{ ref('oliver_dim_product') \}\}  p on f.product_key  = p.product_key
+left join {{ ref('oliver_dim_date') \}\}     d on f.date_key     = d.date_key
+
+9101112131415161781819206734512
+
+Preview
+
+Compile
+
+Build
+
+Format
+Results
+Code quality
+Compiled code
+Lineage$0
 left join {{ ref('oliver_dim_product') }}  p on f.product_key  = p.product_key
 left join {{ ref('oliver_dim_date') }}     d on f.date_key     = d.date_key
